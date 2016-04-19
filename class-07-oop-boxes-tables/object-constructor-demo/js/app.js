181bff755e659d54eb233ccc2bc2882699768938
keiran beaton <keiran beaton>
2016-04-19 10:37:38 -0700

    updated demo

--- class-07-oop-boxes-tables/object-constructor-demo/js/app.js
+++ class-07-oop-boxes-tables/object-constructor-demo/js/app.js
@@ -13,14 +13,20 @@ var jeff = {
   }
 };
 
-// I would need 242 lines of code to model our class this way
-// (22 students * 11 lines each)
-
-// What if... I had a function to make these many similar objects for me? A function where I could pass in a student's unique characteristics as arguments, and then that function would make the individual objects?
+// What if... I had a function to make many similar objects for me? A function where I could pass in a student's unique characteristics as arguments, and then that function would make the individual objects?
 
 // Something like this would then make Jeff:
-var jeff = new Student('Jeff', 10);
+var jeff = new Student('Jeff', 'G', 24);
 
-function(){};
+function Student(firstName, lastInitial, faveNumber){
+  this.course = '201d4';
+  this.firstName = firstName;
+  this.lastInitial = lastInitial;
+  this.faveNumber = faveNumber;
+  this.isCodeNinja = true;
+  this.intro = function () {
+    console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
+  }
+};
 
 // What if I wanted to make an array of students?
